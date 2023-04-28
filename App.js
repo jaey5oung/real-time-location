c2107471c665474a227721e738d75c80292c4924
jaey5oung <jaey5oung@naver.com>
2023-04-28 14:45:33 +0900

    feat background async

--- App.js
+++ App.js
@@ -1,50 +1,59 @@
-import React, { useState, useEffect } from "react"
-import { StyleSheet, Text, View, Button } from "react-native"
-import {
-  requestForegroundPermissionsAsync,
-  watchPositionAsync,
-} from "expo-location"
-import io from "socket.io-client"
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Location from 'expo-location';
+import io from 'socket.io-client';
 
-const SOCKET_URL = "http://211.228.42.81:4000" // WebSocket URL
+const SOCKET_URL = 'http://211.228.42.81:4000'; // WebSocket URL
+const LocationAccuracy = Location.Accuracy; // Define LocationAccuracy
 
 export default function App() {
-  const [socket, setSocket] = useState(null)
-  const [location, setLocation] = useState(null)
+  const [socket, setSocket] = useState(null);
+  const [location, setLocation] = useState(null);
 
   useEffect(() => {
-    ;(async () => {
-      const { status } = await requestForegroundPermissionsAsync()
-      if (status !== "granted") {
-        console.log("Permission to access location was denied")
-        return
+    (async () => {
+      const { status } = await Location.requestBackgroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
       }
-      const location = await watchPositionAsync({}, (location) => {
-        setLocation(location)
-      })
-    })()
-  }, [location])
+
+      const isStarted = await Location.hasStartedLocationUpdatesAsync('background');
+      if (!isStarted) {
+        await Location.startLocationUpdatesAsync('background', {
+          accuracy: LocationAccuracy.Balanced,
+        });
+      }
+
+      const location = await Location.watchPositionAsync({
+        accuracy: LocationAccuracy.Balanced,
+        timeInterval: 5000,
+      }, (location) => {
+        setLocation(location);
+      });
+    })();
+  }, [location]);
 
   useEffect(() => {
     // Connect to WebSocket server
-    const newSocket = io(SOCKET_URL)
-    setSocket(newSocket)
+    const newSocket = io(SOCKET_URL);
+    setSocket(newSocket);
 
-    return () => {
+    return () => { 
       // Disconnect from WebSocket server
-      newSocket.disconnect()
-    }
-  }, [])
+      newSocket.disconnect();
+    };
+  }, []);
 
   useEffect(() => {
     // Send location data to server when location is updated
     if (socket && location) {
-      socket.emit("location", {
+      socket.emit('location', {
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
-      })
+      });
     }
-  }, [socket, location])
+  }, [socket, location]);
 
   return (
     <View style={styles.container}>
@@ -57,80 +66,108 @@ export default function App() {
         <Text>Waiting for location...</Text>
       )}
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-})
+});
 
 
-// ! 디바이스 백그라운드 상태일때 고려
+// import React, { useState, useEffect } from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+// import * as Location from 'expo-location';
+// import io from 'socket.io-client';
 
-// import React, { useState, useEffect } from "react"
-// import { StyleSheet, Text, View } from "react-native"
-// import * as Location from "expo-location"
-// import * as TaskManager from "expo-task-manager"
-// const LOCATION_TASK_NAME = "background-location-task"
-
-// TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
-//   if (error) {
-//     console.log(error)
-//     return
-//   }
-//   if (data) {
-//     const { locations } = data
-//     console.log(locations)
-//     // 업데이트된 위치 정보를 저장하거나 다른 작업 수행
-//   }
-// })
+// const SOCKET_URL = 'http://211.228.42.81:4000'; // WebSocket URL
 
 // export default function App() {
-//   const [location, setLocation] = useState(null)
+//   const [socket, setSocket] = useState(null);
+//   const [location, setLocation] = useState(null);
+
 //   useEffect(() => {
-//     ;(async () => {
-//       const { status } = await Location.requestForegroundPermissionsAsync()
-//       if (status !== "granted") {
-//         console.log("Permission to access location was denied")
-//         return
+//     (async () => {
+//       const { status } = await Location.requestForegroundPermissionsAsync();
+//       if (status !== 'granted') {
+//         console.log('Permission to access location was denied');
+//         return;
 //       }
 
-//       await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-//         accuracy: Location.Accuracy.Highest,
-//         timeInterval: 1000, // 1 seconds
-//         distanceInterval: 1, // 1 meters
-//         deferredUpdatesInterval: 1000, // 1 seconds
-//         foregroundService: {
-//           notificationTitle: "Location Update",
-//           notificationBody: "Your location is being tracked",
-//           notificationColor: "#ff5a5f",
+//       const hasStarted = await Location.hasStartedLocationUpdatesAsync('background');
+//       if (!hasStarted) {
+//         await Location.startLocationUpdatesAsync('background', {
+//           accuracy: Location.Accuracy.Balanced,
+//           distanceInterval: 10,
+//           deferredUpdatesInterval: 1000 * 60 * 5, // 5 minutes
+//           foregroundService: {
+//             notificationTitle: 'Location Tracking',
+//             notificationBody: 'Location tracking is running.',
+//             notificationColor: '#FFFFFF',
+//           },
+//         });
+//       }
+
+//       const location = await Location.getLastKnownPositionAsync();
+//       setLocation(location);
+
+//       const subscriber = await Location.watchPositionAsync(
+//         {
+//           accuracy: Location.Accuracy.Balanced,
+//           timeInterval: 1000,
+//           distanceInterval: 10,
 //         },
-//       })
-//     })()
-//   }, [])
+//         (location) => {
+//           setLocation(location);
+//         },
+//       );
+
+//       return () => subscriber.remove();
+//     })();
+//   }, []);
+
+//   useEffect(() => {
+//     // Connect to WebSocket server
+//     const newSocket = io(SOCKET_URL);
+//     setSocket(newSocket);
+
+//     return () => {
+//       // Disconnect from WebSocket server
+//       newSocket.disconnect();
+//     };
+//   }, []);
+
+//   useEffect(() => {
+//     // Send location data to server when location is updated
+//     if (socket && location) {
+//       socket.emit('location', {
+//         latitude: location.coords.latitude,
+//         longitude: location.coords.longitude,
+//       });
+//     }
+//   }, [socket, location]);
 
 //   return (
 //     <View style={styles.container}>
 //       {location ? (
 //         <>
-//           <Text>위도: {location.coords.latitude}</Text>
-//           <Text>경도: {location.coords.longitude}</Text>
+//           <Text>Latitude: {location.coords.latitude}</Text>
+//           <Text>Longitude: {location.coords.longitude}</Text>
 //         </>
 //       ) : (
 //         <Text>Waiting for location...</Text>
 //       )}
 //     </View>
-//   )
+//   );
 // }
 
 // const styles = StyleSheet.create({
 //   container: {
 //     flex: 1,
-//     alignItems: "center",
-//     justifyContent: "center",
+//     alignItems: 'center',
+//     justifyContent: 'center',
 //   },
-// })
+// });
